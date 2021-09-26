864e376a2ab2255900b6dc6855731d6e2491138a
mcjovial <emmanuelelias455@gmail.com>
2021-09-26 21:30:05 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <h1>Lets build facebook</h1>
+      {/* Header */}
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  )
+}
